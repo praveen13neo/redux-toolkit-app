980e5948840fc4db43b20714a57c5846970242fc
Ashtikar <praveen.ashtikar@capgemini.com>
2022-01-06 14:10:02 +0530

    only redux is implemented in this app but i recommend use redux-toolkit instead redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,53 @@
-import {createSlice, configureStore} from "@reduxjs/toolkit";
+import { createStore } from "redux";
+
+export const INCREMENT = "increment";
+export const INCREASE = "increase";
+export const DECREMENT = "decrement";
+export const TOGGLE = "toggle";
 
 const initialState = { counter: 0, showCounter: true }
 
-const counterSlice = createSlice({
-    name : 'counter',
-    initialState,
-    reducers : {
-        increment(state){
-            state.counter++;
-        },
-        increase(state, action){
-            state.counter = state.counter + action.payload;
-        }, 
-        decrement(state){
-            state.counter--;
-        },
-        toggle(state){
-            state.showCounter = !state.showCounter;
+const counterReducer = (state = initialState, action) => {
+    if (action.type === INCREMENT) {
+        return {
+            counter: state.counter + 1,
+            showCounter: state.showCounter
         }
     }
-})
 
-const store = configureStore({
-    reducer : counterSlice.reducer
-})
+    // user's payloads or parameter passed
+    if (action.type === INCREASE) {
+        return {
+            counter: state.counter + action.value,
+            showCounter: state.showCounter
+
+        }
+    }
 
 
-export const counterActions = counterSlice.actions;
+    if (action.type === DECREMENT) {
+        return {
+            counter: state.counter > 0 ? state.counter - 1 : 0,
+            showCounter: state.showCounter
 
+        }
+    }
+
+
+    if (action.type === TOGGLE) {
+        return {
+            counter: state.counter,
+            showCounter: !state.showCounter
+
+        }
+    }
+
+    return state;
+
+}
+
+
+const store = createStore(counterReducer);
 
 export default store;
 
